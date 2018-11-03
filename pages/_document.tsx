b01aed0d7a8b7991c593bcdbcbfa53c5a2a0d289
Noam Alffasy <noahm1234@outlook.com>
2018-11-03 19:50:19 +0200

    Removed trailing slash

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,7 +9,7 @@ class MyDocument extends Document {
   render() {
     const ASSET_URL =
       process.env.NODE_ENV === "production"
-        ? "https://noamalffasy.github.io/SchoolWebsite/"
+        ? "https://noamalffasy.github.io/SchoolWebsite"
         : "";
 
     return (
