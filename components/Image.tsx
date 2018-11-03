f75da2648b8a93f39d726e234a4415d58c0c2ce1
Noam Alffasy <noahm1234@outlook.com>
2018-11-03 18:54:18 +0200

    Made Image component to show images on background image for consistent width and height without scaling the image

--- components/Image.tsx
+++ components/Image.tsx
@@ -10,22 +10,26 @@ class Image extends Component<Props> {
   render() {
     const { imageSrc, caption } = this.props;
     return (
-      <div className="image">
-        <img src={imageSrc} alt={caption} />
+      <div className="image-outer">
+        <div className="image" />
         <p className="caption">{caption}</p>
         <style jsx>{`
-          .image {
+          .image-outer {
             margin: 1rem 0.5rem;
           }
 
-          .image img {
+          .image-outer .image {
             display: block;
+            background: url("${imageSrc}");
+            background-size: cover;
+            background-position: center center;
             width: 15rem;
+            height: 15rem;
             border-radius: 2px;
             margin: 0 auto;
           }
 
-          .image p.caption {
+          .image-outer p.caption {
             font-size: 0.85rem;
             text-align: center;
           }
