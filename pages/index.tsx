533348b523bace75e7ebf3838cf391b527de985b
Noam Alffasy <noahm1234@outlook.com>
2018-11-03 18:51:38 +0200

    Reworked the categories data

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,33 +2,58 @@ import * as React from "react";
 import { Component } from "react";
 
 import Head from "next/head";
+import Link from "next/link";
 
 import Card, { CardImage } from "../components/Card";
 
-import highlightsData from "../data/highlights";
+import categoriesData from "../data/categories";
 import eventsData from "../data/events";
 
 class Home extends Component {
   render() {
-    const highlights = highlightsData;
+    const categories = categoriesData;
     const events = eventsData;
 
     return (
-      <>
+      <div className="container">
         <Head>
           <title>קריית החינוך פארק המדע</title>
         </Head>
-        <div className="highlights-outer">
+        <div className="categories-outer">
           <h1>מגמות</h1>
-          <div className="highlights">
-            {highlights.map((highlight, i) => {
+          <div className="categories">
+            {categories.map((category, i) => {
               return (
                 <Card key={i}>
-                  <CardImage background={highlight.iconBackground} svg>
-                    {highlight.svg}
+                  <CardImage background={category.iconBackground} svg>
+                    {category.svg}
                   </CardImage>
-                  <h2>{highlight.title}</h2>
-                  {highlight.children}
+                  <h2>{category.title}</h2>
+                  {typeof category.desc === "string" ? (
+                    <>
+                      <p>{category.desc}</p>
+                      <Link
+                        href={`/highlight?id=${category.id}`}
+                        as={`/highlights/${category.id}`}
+                      >
+                        <a>קרא עוד</a>
+                      </Link>
+                    </>
+                  ) : (
+                    category.desc.map((category, i) => {
+                      return (
+                        <div className="sub-category" key={i}>
+                          <p>{category.content}</p>
+                          <Link
+                            href={`/highlight?id=${category.id}`}
+                            as={`/highlights/${category.id}`}
+                          >
+                            <a>קרא עוד</a>
+                          </Link>
+                        </div>
+                      );
+                    })
+                  )}
                 </Card>
               );
             })}
@@ -37,12 +62,18 @@ class Home extends Component {
         <div className="events-outer">
           <h1>אירועים אחרונים</h1>
           <div className="events">
-            {events.map((event, i) => {
+            {events.slice(0, 2).map((event, i) => {
               return (
                 <Card key={i}>
                   <CardImage background={`url("${event.image}")`} />
                   <h2>{event.title}</h2>
-                  {event.children}
+                  <p>{event.desc}</p>
+                  <Link
+                    href={`/event?id=${event.id}`}
+                    as={`/events/${event.id}`}
+                  >
+                    <a>קרא עוד</a>
+                  </Link>
                 </Card>
               );
             })}
@@ -51,21 +82,28 @@ class Home extends Component {
         <style jsx>{`
           h1 {
             color: #3e618f;
-            font-size: 4rem;
+            font-size: 3rem;
             text-align: center;
             margin: 2rem 0 1rem;
           }
 
-          .highlights,
+          .categories,
           .events {
             display: flex;
             margin: 1rem 0 0 0;
             align-items: center;
             justify-content: center;
             flex-flow: wrap;
+            flex-direction: row-reverse;
+          }
+
+          @media (min-width: 768px) {
+            h1 {
+              font-size: 4rem;
+            }
           }
         `}</style>
-      </>
+      </div>
     );
   }
 }
